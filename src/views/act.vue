887847453f6b69514aaa5eef448fcc4e35c70e99
mahuan <420612431@qq.com>
2018-06-12 16:27:00 +0800

    验证

--- src/views/act.vue
+++ src/views/act.vue
@@ -4,14 +4,14 @@
         <div class="conat">
             <div>
                 <span>手机号</span>
-                <input id="mobile" type="number"   @onkeyup="chang()" placeholder="请输入手机号码" >
+                <input id="mobile" v-model="from.mobile" type="number"  placeholder="请输入手机号码" >
             </div>
             <div>
                 <span>手机验证码</span>
-                <input type="text" placeholder="手机验证码" @onkeyup="chang_(this)" >
-                <button  :disabled="disabled"  @click="code" v-model="msg" id="todo" >{{msg}}</button>
+                <input type="text" v-model="from.code" placeholder="手机验证码" @onkeyup="chang_(this)" >
+                <button :class="{ active:!disabled }" :disabled="Togg||!(count==60)"  @click="code" id="todo" >{{msg}}</button>
             </div>
-            <button id="sub">提交</button>
+            <button :class="{ active:!Togg&&!!(from.code) }" :disabled="Togg||!(from.code)" id="sub">提交</button>
         </div>
     </div>
   </div>
@@ -19,122 +19,124 @@
 
 <script>
 export default {
-  name: 'act-view',
+  name: "act-view",
   data() {
     return {
-        msg: "发送验证码" ,
-        disabled: false
+      count: 60,
+      Togg: true,
+      from: {
+        mobile: "",
+        code: ""
+      },
+      msg: "发送验证码",
+      disabled: false,
+      timer: null
+    };
+  },
+  watch: {
+    "from.mobile"(newVal) {
+      var reg = /^1[0-9]\d{9}$/g;
+      if (reg.test(newVal)) {
+        this.Togg = false;
+      } else {
+        this.Togg = true;
+      }
     }
   },
-  watch:{
-        msg:{
-　　　　　　　handler(curVal,oldVal){
-                var _this = this
-　　　　　　　　　　　　console.log(curVal,oldVal)
-                if ( curVal == 0 ) {
-                    _this.msg = "发送验证码"
-                    this.disabled = !this.disabled
-                    return
-                }
-                    var timer = setTimeout(function() {
-                        _this.msg = curVal -- 
-                        
-                        }, 1000)
-　　　　　　　　　　},
-　　　　　　　　　　deep:true
-　　　　　　　　}
-　　　　　　},
-  methods:{
-    code: function () {
-      this.msg = 59  
-      this.disabled = !this.disabled
-    },
-     chang:function() {
-        var reg = /^1[0-9]\d{9}$/g;
-        if (reg.test($('#mobile').val()) && count == 60) {
-            $('#todo').addClass('active');
-            Togg = true;
-        } else {
-            Togg = false;
-            $('#todo').removeClass('active');
+  methods: {
+    code: function() {
+      if (this.disabled) {
+        return;
+      }
+      this.disabled = !this.disabled;
+      this.timer = setInterval(() => {
+        this.msg = this.count--;
+        if (this.count == 0) {
+          clearInterval(this.timer);
+          this.msg = "发送验证码";
+          this.count = 60;
+          this.disabled = !this.disabled;
+          return;
         }
-    }
+      }, 1000);
+    },
+    chang: function() {}
   }
-} 
+};
 </script>
 <style  scoped>
 .box {
-    width: 100%;
-    height: 100%;
-    /*overflow: hidden;*/
+  width: 100%;
+  height: 100%;
 }
 
 .box .conat {
-    width: 678px;
-    height: 178px;
-    background: white;
-    border-radius: 10px;
-    margin-left: 24px;
-    font-size: 28px;
-    margin-top: 24px;
+  width: 678px;
+  height: 178px;
+  background: white;
+  border-radius: 10px;
+  margin-left: 24px;
+  font-size: 28px;
+  margin-top: 24px;
 }
 
 .box .conat div:nth-of-type(1) {
-    width: 655px;
-    height: 90px;
-    line-height: 90px;
-    border-bottom: 1px solid #e4e4e4;
-    margin-left: 20px;
+  width: 655px;
+  height: 90px;
+  line-height: 90px;
+  border-bottom: 1px solid #e4e4e4;
+  margin-left: 20px;
 }
 
 .box .conat div:nth-of-type(1) input {
-    font-size: 28px;
-    outline: none;
-    border: none;
-    margin-left: 120px;
+  font-size: 28px;
+  outline: none;
+  border: none;
+  margin-left: 120px;
 }
 
 .box .conat div:nth-of-type(2) {
-    width: 655px;
-    height: 90px;
-    line-height: 90px;
-    border-bottom: 1px solid #e4e4e4;
-    margin-left: 20px;
-    position: relative;
+  width: 655px;
+  height: 90px;
+  line-height: 90px;
+  border-bottom: 1px solid #e4e4e4;
+  margin-left: 20px;
+  position: relative;
 }
 
 .box .conat div:nth-of-type(2) input {
-    font-size: 28px;
-    outline: none;
-    border: none;
-    margin-left: 64px;
+  font-size: 28px;
+  outline: none;
+  border: none;
+  margin-left: 64px;
 }
 
 #todo {
-    width: 180px;
-    height: 60px;
-    background: #bfbfbf;
-    color: #ffffff;
-    position: absolute;
-    top: -20px;
-    right: 30px;
-    line-height: 60px;
-    text-align: center;
-    border-radius: 10px;
-    font-size:28px;
+  width: 180px;
+  height: 60px;
+  background: #bfbfbf;
+  color: #ffffff;
+  position: absolute;
+  top: -20px;
+  right: 30px;
+  line-height: 60px;
+  text-align: center;
+  border-radius: 10px;
+  font-size: 28px;
 }
 
 .box button {
-    width: 678px;
-    height: 88px;
-    margin-top: 40px;
-    background: #bfbfbf;
-    font-size: 36px;
-    color: white;
-    border-radius: 10px;
-    transition: all .3s;
+  width: 678px;
+  height: 88px;
+  margin-top: 40px;
+  background: #bfbfbf;
+  font-size: 36px;
+  color: white;
+  border-radius: 10px;
+  transition: all 0.3s;
 }
-.box button.active ,#todo.active{
+.box button.active,
+#todo.active {
   background: #2396ff;
 }
 </style>
